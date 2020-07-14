110caa0df76c1c4f55820920d53905a0d6456e6e
Alex Kay <55444371+AlexKay28@users.noreply.github.com>
2020-07-14 13:46:51 +0300

    Add files via upload

---
+++ MyFastText.py
@@ -0,0 +1,30 @@
+from gensim.models.fasttext import FastText as FT_gensim
+from gensim.test.utils import datapath
+
+def create_my_model(path='new_train.csv'):
+    print('Using gensim libraries...')
+    
+    # Set file names for train and test data
+    corpus_file = datapath(path)
+    model = FT_gensim(size=100)
+    
+    print('build the vocabulary...')
+    model.build_vocab(corpus_file=corpus_file)
+    
+    print('train the model...')
+    model.train(
+        corpus_file=corpus_file, 
+        epochs=10,
+        total_examples=model_gensim.corpus_count, 
+        total_words=model_gensim.corpus_total_words,
+        model='skipgram',
+        iter=20,
+        threads=1,
+        min_count=3)
+    
+    model.save('./fasttext_model/model_FT.model')
+    
+if __name__ == "__main__":
+    # execute only if run as a script
+    create_my_model()
+    exit()
